e31f928e15221cdced17a8c3e9a0c3b392e1ecf0
eezJerome <48054365+eezJerome@users.noreply.github.com>
2019-04-22 20:11:15 +1000

    just to back up from my pc

--- GAMarker_main.ino
+++ GAMarker_main.ino
@@ -114,30 +114,37 @@
                 --> implement the single acceleration of the velocity or the double integration of acceleration (depends which one is
                     being output by the accelerometer). Then do mouse.move().
   2019.04.08 -- attempting to add integration of data using 1/2 bh dx
-             -- added commas to data because data splitting can be done on Sheets 
+             -- added commas to data because data splitting can be done on Sheets
              -- found out that with a single stream of data (i.e. one axis) there are about 410-420 data points in a second
                 data stream with 3 accelerometer axes gives about 250 readings in one second
              -- ran Get_Offset_Values_MPU_Calibration and averaged the offsets in 5 trials
                 --> new offsets
                     (reading as gx, gy, gz, ax, ay, az)
-                    -4172.5  -3436  1473.5  50  -39  -18.5 
+                    -4172.5  -3436  1473.5  50  -39  -18.5
              -- during testing the sensor was TILTED on the breadboard....retesting...
   2019.04.12 -- added terminal clear in loop()
   2019.04.13 -- added low-pass filter via DLPF
              -- really considering trapezoidal area to integrate acceleration
   2019.04.15 -- updated offsets because i didn't make sure the sensor was leveled the last time
                 new offsets:
+                *******NOTE THIS IS ONLY FOR MY CURRENT BREADBOARD SETUP
                 (reading as gx, gy, gz, ax, ay, az)
-                4275  -3406 1487  50  -41 -19
+                -4280  -3452 1475  51  -40 -19
+             -- goals for today:
+                --> write some code to find rate of change per axis (deltaX, deltaY, deltaZ)
+  2019.04.22 -- learned how to integrate (thanks organic tutor <3 )
+             -- trying to implement the intergrate() algorithm
 */
 #include <I2Cdev.h>
 #include <MPU6050.h>
+
 //some constants that I had no idea what to put so i copied the constants from electronoobs site
 
 // declaring sensor module
 MPU6050 mpu;
 
 // defining vars for raw values
+const float g = 9.80665;
 int16_t
 rawaccx, rawaccy, rawaccz,
          rawgyrx, rawgyry, rawgyrz;
@@ -149,8 +156,10 @@ uaccx, uaccy,
        ugyrx, ugyry,
        usumx, usumy;
 
-float
-elapsedtime, time, timeprev, currenttime;
+//float
+//time, prevtime, currenttime;
+//float currenttime = micros()/1000000.0f;
+//float prevtime = micros()/1000000.0f;
 
 int i;
 float pi = 180 / 3.141592654;
@@ -161,8 +170,8 @@ const int convraw = 1671;
 //gyro conversion factor
 // gyro might not be so necessary
 const int convdeg = 131;   //0.000031;
-  // this is taken from Joop Brokking video (MPU-6050 6dof IMU tutorial for auto-leveling quadcopters with Arduino source code at 7:48)
-  // i.e. 1 / 250 / 131 (in my sensor it is 131 because of the FS_SEL=0 in the datasheet)
+// this is taken from Joop Brokking video (MPU-6050 6dof IMU tutorial for auto-leveling quadcopters with Arduino source code at 7:48)
+// i.e. 1 / 250 / 131 (in my sensor it is 131 because of the FS_SEL=0 in the datasheet)
 
 
 //real values
@@ -177,58 +186,48 @@ void setup() {
   Serial.begin(115200);
 
   mpu.initialize();
-// old offsets
-//  mpu.setXGyroOffset(50); //this was at 220
-//  mpu.setYGyroOffset(-41); //76
-//  mpu.setZGyroOffset(-19); //-85
-//  //these are ones I put
-//  mpu.setXAccelOffset(-4275);
-//  mpu.setYAccelOffset(-3406);
-//  mpu.setZAccelOffset(1487);
 
-// new offsets as of 2019.04.08
-// taken when sensor was set on flat ground, warmed up, and precise calibration (or as precise as I could have made it)
-  mpu.setXGyroOffset(50); //this was at 220
-  mpu.setYGyroOffset(-39); //76
-  mpu.setZGyroOffset(-18); //-85
+  //  51  -40 -19 -4280  -3452 1475
+  mpu.setXGyroOffset(51); //this was at 220
+  mpu.setYGyroOffset(40); //76
+  mpu.setZGyroOffset(-19); //-85
   //these are ones I put
-  mpu.setXAccelOffset(-4172);
-  mpu.setYAccelOffset(-3436);
-  mpu.setZAccelOffset(1473);
+  mpu.setXAccelOffset(-4280);
+  mpu.setYAccelOffset(-3452);
+  mpu.setZAccelOffset(1475);
 
-  mpu.setDLPFMode(MPU6050_DLPF_BW_98);
+  mpu.setDLPFMode(MPU6050_DLPF_BW_42); // 42hz digital low pass filter
 
-  Serial.println("This program converts the raw MPU6050 values in terms of m/s/s/");
-  Serial.println("3");
-  delay(1000);
-  Serial.println("2");
-  delay(1000);
-  Serial.println("1");
-  delay(1000);
+  //  Serial.println("This program converts the raw MPU6050 values in terms of m/s/s/");
+  //  Serial.println("3");
+  //  delay(1000);
+  //  Serial.println("2");
+  //  delay(1000);
+  //  Serial.println("1");
+  //  delay(1000);
 }
 
 void loop() {
-currenttime = millis();
-//  while(millis() <= 4001){ // this is to test individual axes for data collecting/analysis; comment as you wish
+  //  while(millis() <= 1001){ // this is to test individual axes for data collecting/analysis; comment as you wish
   mpu.getMotion6(&rawaccx, &rawaccy, &rawaccz, &rawgyrx, &rawgyry, &rawgyrz);
-  /*float vals =*/ getaccel(rawaccx, rawaccy, rawaccz);  // just to print this function
+  getaccel(rawaccx, rawaccy, rawaccz);  // just to print this function
   getgyr(rawgyrx, rawgyry, rawgyrz);
   // getangle(); // still don't know what to put here....I'll need to look into datasheet or something
   // gettime();
 
-// the data output can then be pasted onto a spreadsheet. Split data for individual axix; use space as the separator
-/*  Serial.print("X:  ");*/ Serial.print(realaccx); Serial.print(" \t"); // m/s2   |  ");
-/*  Serial.print("Y:  ");*/ Serial.print(realaccy); Serial.print("      \t"); // m/s2   |   ");
-/*  Serial.print("Z:  ");*/ Serial.print(realaccz); Serial.print("      \t"); // m/s2   |   ");
-Serial.print((currenttime - 1000) / 1000); Serial.print("         \t");
-//  Serial.print("oX:  "); Serial.print(realdegx); Serial.print("\t o/s   |   ");
-//  Serial.print("oY:  "); Serial.print(realdegy); Serial.print("      \t o/s   |   ");
-//  Serial.print("oZ:  "); Serial.print(realdegz); Serial.print("      \t o/s   |   ");
-  Serial.println(" ");
-//  Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"); // use this when debugging
+  // the data output can then be pasted onto a spreadsheet. Split data for individual axix; use space as the separator
+  //  currenttime = millis();
 
-//  } // comment this if no while loop 
-  timeprev = currenttime; // this is for finding the time difference
+  //  Serial.print(realaccx); Serial.print(" \t");
+  //  Serial.print(realaccy); Serial.print("      \t");
+  //  Serial.print(realaccz); Serial.print("      \t");
+
+    accel_getDelta();
+  //Serial.print((currenttime - 1000) / 1000); Serial.print("         \t");
+  //  Serial.println(" ");
+
+  //  } // comment this if no while loop
+  //  prevtime = currenttime; // this is for finding the time difference
 }
 
 
@@ -254,29 +253,80 @@ void getangle() {
 // AngleX = PrevAngleX + GyroDataX * elapsedTime
 
 int gettime() {
-//  // im not so keen on making timers so I copied this
-//  timeprev = time;  //the previous time is stored before the actual time read
-//  time = millis();  // actual time read
+  //  // im not so keen on making timers so I copied this
+  //  timeprev = time;  //the previous time is stored before the actual time read
+  //  time = millis();  // actual time read
+  //
+  //  elapsedtime = (time - timeprev) / 1000;
+}
+
+float integrate() {
+  /*
+     this uses the area of a triangle to basically integrate the area under the curve (i.e. the sensor readings)
+     integrate each axis for usable data
+     take note of sample time
+     algorithm in a nutshell:
+        -- take current time
+        -- measure 1/2bh dx, where "b" is half of the time (measured in s), h is the y-value, and not sure how dx falls into all of this...
+        -- if i can learn how to really integrate, then error can be removed
+           **** 1/2 (a+b) h ***
+              it would make sense to reason the following
+              -- idk i really think now that it won't matter....
+    NO, USE TRAPEZOID INSTEAD OF TRIANGLE!
+  */
+
+
+}
+
+// not sure if i'll do it right
+// The intent of this function is to get how much acceleration changes
+// hence "getDelta"
+// time, prevtime, currenttime
+
+int sampletime = 50; 
+
+//float accel_getDelta() {
+//  currenttime = micros();
+//  float x1 = realaccx;
+//  float y1 = realaccy;
+//  float z1 = realaccz;
 //
-//  elapsedtime = (time - timeprev) / 1000;
+//  float deltat = (currenttime - prevtime);
+//  if (micros()  > sampletime + currenttime) {
+//    float x2 = realaccx - x1;
+//    float y2 = realaccy - y1;
+//    float z2 = realaccz - z1;   
+//    Serial.print(x2); Serial.print("\t  ");
+//    Serial.print(y2); Serial.print("\t  ");
+//    Serial.print(z2); Serial.print("\t");
+//    Serial.println(currenttime); Serial.print("\t");
+//    prevtime = currenttime;
+//  }
+//}
+//
+//void mouse_test(){
+//  Mouse.move(readAxis(0), readAxis(1), 0);
+//
+//}
+//
+unsigned long prevtime = 0;
+
+long accel_getDelta(){
+  static long x1=0, y1=0, z1=0;
+  unsigned long currenttime = micros();//1000000.0f;
+  float deltat = (currenttime - prevtime);
+
+  if (deltat > sampletime){
+    prevtime = currenttime;
+    
+    Serial.print(realaccx - x1); Serial.print("\t ");  
+    Serial.print(realaccy - y1); Serial.print("\t ");
+    Serial.print(realaccz - z1); Serial.print("\t ");
+    Serial.println(currenttime); Serial.print("\t ");
+    Serial.println(" ");
+
+    x1 = realaccx;
+    y1 = realaccy;
+    z1 = realaccz;
+  }
 }
-
-float integrate(){
-/*  
-   this uses the area of a triangle to basically integrate the area under the curve (i.e. the sensor readings)
-   integrate each axis for usable data
-   take note of sample time
-   algorithm in a nutshell:
-      -- take current time
-      -- measure 1/2bh dx, where "b" is half of the time (measured in s), h is the y-value, and not sure how dx falls into all of this...
-      -- if i can learn how to really integrate, then error can be removed
-         **** 1/2 (a+b) h ***
-         *  it would make sense to reason the following 
-         *  -- 
-  NO, USE TRAPEZOID INSTEAD OF TRIANGLE!
-*/     
-  
-
-}
-
-
